e9bb614a0afd853fd921dbfd885c1b40ebd0f3c0
Taher Ayadi <taher.ayadi@esprit.tn>
2024-11-14 19:44:47 +0100

    trouchik

--- User/forms.py
+++ User/forms.py
@@ -1,32 +1,23 @@
 # User/forms.py
-from django import forms
-from .models import User, Client, Professional, Provider, Admin
-from django.contrib.auth.forms import UserCreationForm
 
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'age', 'gender', 'address', 'phone']
+from .models import Client, Professional, Provider
+from django.contrib.auth.forms import UserCreationForm
 
 class ClientForm(UserCreationForm):
     class Meta:
         model = Client
         fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'age', 'gender', 'address', 'phone', 'weight', 'height', 'bmi']
 
-class ProfessionalForm(UserForm):
-    class Meta(UserForm.Meta):
+class ProfessionalForm(UserCreationForm):
+    class Meta:
         model = Professional
-        fields = UserForm.Meta.fields + ['years_of_experience', 'diploma', 'cin', 'matricule']
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'age', 'gender', 'address', 'phone', 'years_of_experience', 'diploma', 'cin', 'matricule']
 
-class ProviderForm(UserForm):
-    class Meta(UserForm.Meta):
+class ProviderForm(UserCreationForm):
+    class Meta:
         model = Provider
-        fields = UserForm.Meta.fields + ['company_name', 'tax_code']
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'age', 'gender', 'address', 'phone','company_name', 'tax_code']
 
-class AdminForm(UserForm):
-    class Meta(UserForm.Meta):
-        model = Admin
-        fields = UserForm.Meta.fields
 
 
 
